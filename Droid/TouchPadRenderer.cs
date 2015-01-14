e756597c8c221ae1b1e94495085a34528c80a07b
Viktor Gustavsson <villor94@gmail.com>
2015-01-14 21:52:42 +0100

    Fixed compilation error

--- Droid/TouchPadRenderer.cs
+++ Droid/TouchPadRenderer.cs
@@ -17,21 +17,16 @@ namespace StyrClient.Droid
 		{
 			_listener = new TouchPadGestureListener ();
 			_detector = new GestureDetector (_listener);
-
 		}
 
 		protected override void OnElementChanged (ElementChangedEventArgs<BoxView> e)
 		{
 			base.OnElementChanged (e);
 
-			/*if (e.NewElement == null) {
-				if (this.GenericMotion != null) {
-					this.GenericMotion -= HandleGenericMotion;
-				}
-				if (this.Touch != null) {
-					this.Touch -= HandleTouch;
-				}
-			}*/
+			if (e.NewElement == null) {
+				this.GenericMotion -= HandleGenericMotion;
+				this.Touch -= HandleTouch;
+			}
 
 			if (e.OldElement == null) {
 				this.GenericMotion += HandleGenericMotion;
