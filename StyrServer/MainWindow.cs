b1dcd327983032feffb6a61ef40a2d771243534d
Viktor Gustavsson <villor94@gmail.com>
2015-01-14 20:37:20 +0100

    Added Server class with discovery functionality

--- StyrServer/MainWindow.cs
+++ StyrServer/MainWindow.cs
@@ -1,15 +1,32 @@
 ﻿using System;
+using System.Text;
 using Gtk;
+
+using StyrServer;
+
+class Logger : System.IO.TextWriter {
+	private TextView txt;
+	public Logger(TextView txt) { this.txt = txt; }
+	public override Encoding Encoding { get { return null; } }
+	public override void Write(char value) {
+		if (value != '\r') txt.Buffer.Text += new string(value, 1);
+	}
+}
 
 public partial class MainWindow: Gtk.Window
 {
+	private Server server;
+
 	public MainWindow () : base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
+		Console.SetOut (new Logger (textview1));
+		server = new Server (1337);
 	}
 
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
 	{
+		server.Running = false;
 		Application.Quit ();
 		a.RetVal = true;
 	}
---
+++ StyrServer/Server.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+
+namespace StyrServer
+{
+	public class Server
+	{
+		enum PacketType : byte {
+			Discovery,
+			Offer,
+			Connection
+		};
+
+		private UdpClient udpClient;
+		private IPEndPoint groupEP;
+
+		public bool Running { get; set; }
+
+		public Server (int port)
+		{
+			udpClient = new UdpClient (port);
+			groupEP = new IPEndPoint (IPAddress.Any, port);
+
+			Running = true;
+
+			Thread mainThread = new Thread (ServerLoop);
+			mainThread.Start ();
+			Console.WriteLine ("Server running on port {0}", port);
+		}
+
+		private void ServerLoop()
+		{
+			byte[] receivedPacket;
+			while (Running) {
+				if (udpClient.Available > 0) {
+					receivedPacket = udpClient.Receive (ref groupEP);
+					if (receivedPacket.Length > 0) {
+						switch (receivedPacket [0]) {
+						case (byte)PacketType.Discovery:
+							Console.WriteLine ("Discovery received from {0}:{1}", groupEP.Address, groupEP.Port);
+							byte[] offer = { (byte)PacketType.Offer };
+							udpClient.Send (offer, offer.Length, groupEP);
+							break;
+						case (byte)PacketType.Connection:
+							Console.WriteLine ("Connection received");
+							break;
+						default:
+							Console.WriteLine ("Unknown packet received");
+							break;
+						}
+					}
+				}
+			}
+			udpClient.Close ();
+		}
+	}
+}
+
+
