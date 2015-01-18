4a36d970572a4e8524b510071f477bfa3b827b55
Viktor Gustavsson <villor94@gmail.com>
2015-01-18 18:58:16 +0100

    Linked local mouse movement with remote UDP packets.

--- StyrServer/Server.cs
+++ StyrServer/Server.cs
@@ -6,6 +6,8 @@ using System.Threading;
 using System.Diagnostics;
 using System.Collections.Generic;
 
+using StyrServer.Input;
+
 namespace StyrServer
 {
 	public class Server
@@ -22,6 +24,8 @@ namespace StyrServer
 		private UdpClient udpClient;
 		private IPEndPoint groupEP;
 
+		private IMouseDriver mouse;
+
 		public bool Running { get; set; }
 
 		private List<Client> connectedClients;
@@ -34,6 +38,8 @@ namespace StyrServer
 			connectedClients = new List<Client> ();
 
 			Running = true;
+
+			mouse = MouseDriverOSX ();
 
 			Thread mainThread = new Thread (ServerLoop);
 			mainThread.Start ();
@@ -63,8 +69,20 @@ namespace StyrServer
 							break;
 
 						case (byte)PacketType.MouseMovement:
-							if (connectedClients.Exists (p => p.EndPoint == groupEP)) {
-								Debug.WriteLine ("Mouse moved!");
+							if (connectedClients.Exists (p => p.EndPoint.Equals(groupEP)) && receivedPacket.Length == 9) {
+								byte[] xArr = new byte[4];
+								byte[] yArr = new byte[4];
+
+								Array.Copy (receivedPacket, 1, xArr, 0, 4);
+								Array.Copy (receivedPacket, 5, yArr, 0, 4);
+
+								if (BitConverter.IsLittleEndian) {
+									Array.Reverse (xArr);
+									Array.Reverse (yArr);
+								}
+
+								Debug.WriteLine ("X: {0}, Y: {1}", BitConverter.ToSingle (xArr, 0), BitConverter.ToSingle (yArr, 0));
+								mouse.RelativeMove (BitConverter.ToSingle (xArr, 0), BitConverter.ToSingle (yArr, 0));
 							}
 							break;
 
