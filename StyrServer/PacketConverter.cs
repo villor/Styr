074b6cbbb5d603a631c724f704cbb5b4a94aa5eb
Viktor Gustavsson <villor94@gmail.com>
2015-01-23 15:18:54 +0100

    Moved endianess code to its own method for better abstraction.

--- StyrServer/PacketConverter.cs
+++ StyrServer/PacketConverter.cs
@@ -4,25 +4,23 @@ namespace StyrServer
 {
 	public static class PacketConverter
 	{
+		private static void fixEndianess(byte[] array) {
+			if (BitConverter.IsLittleEndian) {
+				Array.Reverse (array);
+			}
+		}
+
 		public static ushort GetUShort(byte[] array, int index)
 		{
 			byte[] shortArr = { array[index], array[index + 1] };
-
-			if (BitConverter.IsLittleEndian) {
-				Array.Reverse (shortArr);
-			}
-
+			fixEndianess (shortArr);
 			return BitConverter.ToUInt16 (shortArr, 0);
 		}
 			
 		public static void PutUShort(ushort val, byte[] array, int index)
 		{
 			byte[] ushortArr = BitConverter.GetBytes(val);
-
-			if (BitConverter.IsLittleEndian) {
-				Array.Reverse(ushortArr);
-			}
-
+			fixEndianess (ushortArr);
 			Array.Copy (ushortArr, 0, array, index, 2);
 		}
 
@@ -30,11 +28,7 @@ namespace StyrServer
 		{
 			byte[] floatArr = new byte[4];
 			Array.Copy (array, index, floatArr, 0, 4);
-
-			if (BitConverter.IsLittleEndian) {
-				Array.Reverse (floatArr);
-			}
-
+			fixEndianess (floatArr);
 			return BitConverter.ToSingle (floatArr, 0);
 		}
 
