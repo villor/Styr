936fd332d2a6bec9afc700169a7d225846b303dc
Viktor Gustavsson <villor94@gmail.com>
2016-07-09 21:24:04 +0200

    Turned StyrServer.Mac into background/statusbar app

---
+++ StyrServer/StyrServer.Mac/App.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+using MonoMac.Foundation;
+using MonoMac.AppKit;
+using MonoMac.ObjCRuntime;
+
+namespace StyrServer.Mac
+{
+	[MonoMac.Foundation.Register("AppDelegate")]
+	public class App : NSApplicationDelegate
+	{
+		Server server;
+
+		public App()
+		{
+		}
+
+		public override void FinishedLaunching(NSObject notification)
+		{
+			// Create a Status Bar Menu
+			NSStatusBar statusBar = NSStatusBar.SystemStatusBar;
+
+			var item = statusBar.CreateStatusItem(30);
+			item.Title = "Styr";
+			item.HighlightMode = true;
+			item.Menu = new NSMenu("Styr");
+
+			var quit = new NSMenuItem("Quit");
+			quit.Activated += (sender, e) =>
+			{
+				NSApplication.SharedApplication.Terminate(this);
+			};
+			item.Menu.AddItem(quit);
+
+			server = new Server(1337);
+		}
+	}
+}
+
