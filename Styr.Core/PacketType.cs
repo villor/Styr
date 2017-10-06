14cf358f471e33a7d4751c87a420fc7fc210f92c
Viktor Gustavsson <villor94@gmail.com>
2017-10-06 20:53:26 +0200

    Added PacketType

---
+++ Styr.Core/PacketType.cs
@@ -0,0 +1,23 @@
+﻿namespace Styr.Core
+{
+	public enum PacketType : byte
+	{
+		Discovery,
+		Offer,
+		Connection,
+		Disconnect,
+		Ack,
+		Ping,
+		AccessDenied,
+		MouseMovement,
+		MouseScroll,
+		MouseLeftClick,
+		MouseLeftUp,
+		MouseLeftDown,
+		MouseRightClick,
+		MouseRightUp,
+		MouseRightDown,
+		InputCharacter,
+		KeyPress
+	}
+}
